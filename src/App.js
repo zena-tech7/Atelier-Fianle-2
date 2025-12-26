0a2240b6bcff7523c2257b9585c4d31eaa4d0e4a
assmaid <159800116+assmaid@users.noreply.github.com>
2025-12-26 15:52:24 +0100

    modification de app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import './App.css';
-import Form from './components/Form/Form';
+import { useState } from "react";
+import Form from "./components/Form/Form";
+import Table from "./components/Table/Table"
+import invest from "./assets/investment-calculator-logo.png"
 
-function App() {
+const App = () => {
+  const [results, setResults] = useState([]);
+
+  const handleFormData = (data) => {
+    // 🔍 extraction des valeurs
+    const {
+      currentSavings,
+      yearlySavings,
+      expectedInterest,
+      duration,
+    } = data;
+
+    let calculatedResults = [];
+    let currentSaving = currentSavings;
+
+    for (let year = 1; year <= duration; year++) {
+      const yearlyInterest =
+        currentSaving * (expectedInterest / 100);
+
+      const savingEndOfYear =
+        currentSaving + yearlyInterest + yearlySavings;
+
+      const investedCapital =
+        currentSavings + yearlySavings * year;
+
+      const totalInterest =
+        savingEndOfYear - investedCapital;
+
+      calculatedResults.push({
+        year,
+        totalSavings: savingEndOfYear,
+        interestYear: yearlyInterest,
+        totalInterest,
+        investedCapital,
+      });
+
+      currentSaving = savingEndOfYear;
+    }
+
+    setResults(calculatedResults);
+  };
+console.log(results);
   return (
-    <div className="App">
-        <Form/>
+    <div>
+      <header>
+      <img  className="img" src={invest} alt="img"/>
+      <h2>Investment calculator </h2>
+      </header>
+      <Form onDataChange={handleFormData} />
+      <Table/>
+      
     </div>
   );
-}
+};
 
 export default App;
